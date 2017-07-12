65d1cbf325b51222a3b3fc8f39bf5d872c1a2fec
Дмитрий Смаглюк <loadergo@gmail.com>
2017-07-12 21:39:26 +0300

    UI update

---
+++ Assets/Scripts/Server/ServerClient.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using BestHTTP.SocketIO;
+using UnityEngine;
+
+namespace Assets.Scripts.Server
+{
+    public class ServerClient : MonoBehaviour
+    {
+        public Action<Data> OnReceiveData { get; set; }
+
+        private SocketManager _manager;
+
+
+        private void Start()
+        {
+            _manager = new SocketManager(new Uri("http://localhost:3000//socket.io/"));
+            _manager.Socket.On("send data", OnSendData);
+        }
+    
+        void OnDestroy()
+        {
+            _manager.Close();
+        }
+
+        public void OnSendData(Socket soket, Packet packet, params object[] args)
+        {
+            Debug.Log(args[0]);
+            string json = args[0].ToString();
+            Data data = JsonUtility.FromJson<Data>(json);
+            if (OnReceiveData != null)
+            {
+                OnReceiveData.Invoke(data);
+            }
+            else
+            {
+                Debug.Log("Data can't be received cause action was not created yet.");
+            }
+        }
+
+
+        public void SendData(Data data)
+        {
+            string json = JsonUtility.ToJson(data);
+            _manager.Socket.Emit("send data", json);
+        }
+
+    }
+}
